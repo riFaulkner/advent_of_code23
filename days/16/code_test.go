0d6233d3b054df252460c5511a2d69be9feff512
richardfaulkner-qz <richard.faulkner@quizlet.com>
2023-12-17 14:03:08 -0700

    day 16

---
+++ days/16/code_test.go
@@ -0,0 +1,45 @@
+package _16
+
+import "testing"
+
+func TestProblem1(t *testing.T) {
+	type args struct {
+		inputFileName string
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{"Example", args{"example.txt"}, 46},
+		{"Puzzle 1", args{"puzzle_work.txt"}, 7632},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := Problem1(tt.args.inputFileName); got != tt.want {
+				t.Errorf("Problem1() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestProblem2(t *testing.T) {
+	type args struct {
+		inputFileName string
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{"Example", args{"example.txt"}, 51},
+		{"Puzzle 2", args{"puzzle_work.txt"}, 8023},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := Problem2(tt.args.inputFileName); got != tt.want {
+				t.Errorf("Problem2() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
